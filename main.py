a2fb0d20a492a3d797fc845725a4b16a5498991e
tanvii22 <2005tanvik@gmail.com>
2026-06-30 12:14:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+load_dotenv()  # Load environment variables from .env file
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins for simplicity; adjust as needed
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class PromptRequest(BaseModel):
+    prompt: str
+
+@app.post("/generate")
+def generate_response(request: PromptRequest):
+    response = model.generate_content(request.prompt)
+    return {
+        "response": response.text
+    }
